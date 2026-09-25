74c5da93e47790e4848aaea4e968875e25da16ce
Yashwin <yashwin.sharma@digixito.org>
2026-09-25 15:40:20 +0530

    add MetaData

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import HomeLayout from "@/components/layouts/home";
-import { Box } from "@mui/material";
 import { getMetadata } from "@/utils/metadata";
+import { Box } from "@mui/material";
 import { Metadata } from "next";
-
 export const metadata: Metadata = getMetadata("/");
 
 export default function Home() {
